e5c5133e5154804d78f0a895ec25ccf2d4f2c0eb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 20:04:40 -0300

    first commit

---
+++ ms-pedidos/src/test/java/com/github/lara/sodre/ms/pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lara.sodre.ms.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
